c238a678aa48d82eff0dbacd680a0e8df5d41264
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-06 01:05:19 -0400

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -1,17 +1,36 @@
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa un  link de youtube`, m, rcanal)
+let handler = async (m, { conn, text }) => {
+  if (!text) {
+    return m.reply("🤍 Por favor, ingresa una URL válida de YouTube.")
+  }
+    await m.react('🕓')
 
-try {
-let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp4?url=${text}`)).json()
-let dl_url = api.data.dl
+  let ytUrlRegex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/;
+  if (!ytUrlRegex.test(text)) {
+    return m.reply("❀ La URL ingresada no es válida. Asegúrate de que sea un enlace de YouTube.")
+  }
 
-await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: null }, { quoted: m })
-} catch (error) {
-console.error(error)
-}}
+  try {
+    let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp4?apikey=gifted&url=${text}`)
+    let json = await api.json()
+    let { quality, title, download_url } = json.result
 
-handler.command = ['ytmp4']
+    await m.react('✅')
+    await conn.sendMessage(m.chat, { 
+      video: { url: download_url }, 
+      caption: `_${title}_`, 
+      mimetype: 'video/mp4', 
+      fileName: `${title}.mp4` 
+    }, { quoted: m })
+  } catch (error) {
+    console.error(error)
+    m.reply("❀ Hubo un error al procesar la URL. Inténtalo nuevamente.")
+  }
+}
+
+handler.help = ['ytmp4 *<link yt>*']
+handler.tags = ['dl']
+handler.command = ['ytmp4', 'ytv', 'fgmp4']
 
 export default handler
