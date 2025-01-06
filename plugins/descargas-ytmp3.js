dad0863164566bca43200b47e437f76fd294c5f7
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-06 01:10:33 -0400

    Actualizar descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+/* import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) return conn.reply(m.chat, `❀ Ingresa un  link de youtube`, m)
@@ -14,5 +14,43 @@ console.error(error)
 }}
 
 handler.command = ['ytmp3']
+
+export default handler */
+
+
+/* 
+
+[ Canal Principal ] :
+https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
+
+[ Canal Rikka Takanashi Bot ] :
+https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
+
+[ Canal StarlightsTeam] :
+https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
+
+[ HasumiBot FreeCodes ] :
+https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
+*/
+
+// *[ ❀ YTMP3 ]*
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text }) => {
+if (!text) {
+return m.reply("❀ Ingresa un link de youtube")
+}
+    
+try {
+let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp3?apikey=gifted&url=${text}`)
+let json = await api.json()
+let { quality, title, download_url } = json.result
+
+await conn.sendMessage(m.chat, { audio: { url: download_url }, fileName: `${title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })
+} catch (error) {
+console.error(error)
+}}
+
+handler.command = /^(ytmp3)$/i
 
 export default handler
