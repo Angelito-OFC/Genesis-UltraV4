9ac44ddbbf1c7a5528e182c71b59122f17c7a013
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-06 00:56:07 -0400

    Actualizar descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -1,9 +1,8 @@
-// *[ ❀ YTMP3 ]*
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) return conn.reply(m.chat, `❀ Ingresa un  link de youtube`, m)
-    
+
 try {
 let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp3?url=${text}`)).json()
 let dl_url = api.data.dl
