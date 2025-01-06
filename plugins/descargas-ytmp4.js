d7fefa22313718ee132392188ccb7336876a83a1
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-06 00:33:24 -0400

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -1,4 +1,5 @@
 /* 
+❀ By JTxs
 
 [ Canal Principal ] :
 https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
@@ -13,20 +14,38 @@ https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
 https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
 */
 
-
 // *[ ❀ YTMP4 ]*
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa un  link de youtube`, m, rcanal)
+let [url, resolution] = text.split(' ')
+if (!url) {
+return conn.reply(m.chat, `Ingresa el link de un video de youtube y una calidad ejemplo : ${usedPrefix + command} + *link* *360* `, m)
+}
     
 try {
-let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp4?url=${text}`)).json()
-let dl_url = api.data.dl
+let apiinfo = await fetch(`https://ytdownloader.nvlgroup.my.id/info?url=${url}`);
+let jsoninfo = await apiinfo.json()
+let titulo = jsoninfo.title
+let duracion = jsoninfo.duration || '-'
+let calidad = resolution || '360'
+let img = jsoninfo.thumbnail
+let dl_url = `https://ytdownloader.nvlgroup.my.id/download?url=${url}&resolution=${calidad}`
+let vidFetch = await fetch(dl_url)
+let video = await vidFetch.buffer()
+let Tamaño = video.length / (1024 * 1024)
 
-await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: null }, { quoted: m })
+let HS = `- *Titulo* : ${titulo}
+- *Link* : ${url}
+- *Duracion* : ${duracion}
+- *Calidad* ${calidad}`
+if (Tamaño > 100) {
+await conn.sendMessage(m.chat, { document: video, caption: HS, mimetype: 'video/mp4', fileName: `${titulo}.mp4`})
+} else {
+await conn.sendMessage(m.chat, { video: video, caption: HS, mimetype: 'video/mp4'})
+}
 } catch (error) {
-console.error(error)
+console.error(error)    
 }}
 
 handler.command = ['ytmp4']
