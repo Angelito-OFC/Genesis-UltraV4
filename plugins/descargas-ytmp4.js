e37c9d8ed32e794a6cd2f8764ffcf0a630bac1eb
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-01-06 00:36:34 -0400

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -1,53 +1,21 @@
-/* 
-❀ By JTxs
-
-[ Canal Principal ] :
-https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
-
-[ Canal Rikka Takanashi Bot ] :
-https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
-
-[ Canal StarlightsTeam] :
-https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
-
-[ HasumiBot FreeCodes ] :
-https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
-*/
-
-// *[ ❀ YTMP4 ]*
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-let [url, resolution] = text.split(' ')
-if (!url) {
-return conn.reply(m.chat, `Ingresa el link de un video de youtube y una calidad ejemplo : ${usedPrefix + command} + *link* *360* `, m)
-}
-    
+import fetch from "node-fetch"
+ 
+let handler = async (m, { text, conn, args, usedPrefix, command }) => {
+if (!args[0]) return m.reply("ingresa un link de youtube")
+ 
 try {
-let apiinfo = await fetch(`https://ytdownloader.nvlgroup.my.id/info?url=${url}`);
-let jsoninfo = await apiinfo.json()
-let titulo = jsoninfo.title
-let duracion = jsoninfo.duration || '-'
-let calidad = resolution || '360'
-let img = jsoninfo.thumbnail
-let dl_url = `https://ytdownloader.nvlgroup.my.id/download?url=${url}&resolution=${calidad}`
-let vidFetch = await fetch(dl_url)
-let video = await vidFetch.buffer()
-let Tamaño = video.length / (1024 * 1024)
-
-let HS = `- *Titulo* : ${titulo}
-- *Link* : ${url}
-- *Duracion* : ${duracion}
-- *Calidad* ${calidad}`
-if (Tamaño > 100) {
-await conn.sendMessage(m.chat, { document: video, caption: HS, mimetype: 'video/mp4', fileName: `${titulo}.mp4`})
-} else {
-await conn.sendMessage(m.chat, { video: video, caption: HS, mimetype: 'video/mp4'})
-}
+let api = await fetch(`https://api.agatz.xyz/api/ytmp4?url=${args[0]}`)
+let json = await api.json()
+let { title, thumbnail, quality, downloadUrl:dl_url } = json.data
+ 
+let JT = `*Titulo :* ${title}
+*Calidad :* ${quality}`
+ 
+await conn.sendFile(m.chat, dl_url, 'HasumiBotFreeCodes.mp4', JT, m)
 } catch (error) {
-console.error(error)    
+console.error(error)
 }}
-
+ 
 handler.command = ['ytmp4']
-
+ 
 export default handler
